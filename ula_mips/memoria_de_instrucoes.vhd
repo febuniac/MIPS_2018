6d260a77e312a357332fb4986a2e8e63a73ea727
Carlos Rosa <kdu.carloseduardo@gmail.com>
2018-11-06 15:02:35 -0200

    Atualização memoria de intruções.

--- ula_mips/memoria_de_instrucoes.vhd
+++ ula_mips/memoria_de_instrucoes.vhd
@@ -36,21 +36,21 @@ architecture rtl of memoria_de_instrucoes is
 --			tmp(addr_pos) := std_logic_vector(to_unsigned(addr_pos, DATA_WIDTH));
 --		end loop;
 		tmp(0) := "00000000000000000000000000000000"; -- nop
-		tmp(1) := b"100011_00000_00010_0000000000000001"; -- poe em r2 o que estava na memoria r0 + 1
-		tmp(2) := b"100011_00000_00001_0000000000000100"; -- poe em r1 o que estava na memoria r0 + 4
+		tmp(1) := b"100011_00000_00010_0000000000000100"; -- poe em r2 o que estava na memoria r0 + 1 (refere a byte 4)
+		tmp(2) := b"100011_00000_00001_0000000000010000"; -- poe em r1 o que estava na memoria r0 + 4 (refere a byte 16)
 		tmp(3) := b"000000_00001_00001_00011_00000_100000"; -- r1+r1 no r3 (add)
 		--tmp(4) := b"000000_00001_00010_00100_00000_100010"; --r1-r2 no r4 (sub)
 		--tmp(5) := b"000000_00001_00010_00101_00000_100100"; --r1 and r2 no r5 (and)
 		--tmp(6) := b"000000_00001_00010_00110_00000_100101"; --r1 or r2 no r6 (or)
 		--tmp(7) := b"000000_00010_00001_00111_00000_101010"; --r1 slt r2 no r7 (slt)
-		tmp(4) := b"000100_00000_00000_0000000000000111"; -- beq r0 e r0
+		tmp(4) := b"000100_00000_00000_0000000000011100"; -- beq r0 e r0
 		--tmp(9) := b"000010_00000000000000000000001000"; -- jmp para 8
-		--tmp(10) := b"101011_00000_00001_0000000000000101"; --sw pego o conteudo de r1 e coloco em r0 + 5 
-		--tmp(11) := b"101011_00000_00011_0000000000000110"; --sw pego o conteudo de r3 e coloco em r0 + 6 
-		--tmp(12) := b"100011_00000_00010_0000000000000101"; -- lw pego o conteudo de r0 + 5 e coloco em r2
-		--tmp(13) := b"100011_00000_00100_0000000000000110"; -- lw pego o conteudo de r0 + 6 e coloco em r4
+		--tmp(10) := b"101011_00000_00001_0000000000010100"; --sw pego o conteudo de r1 e coloco em r0 + 5 
+		--tmp(11) := b"101011_00000_00011_0000000000001100"; --sw pego o conteudo de r3 e coloco em r0 + 6 
+		--tmp(12) := b"100011_00000_00010_0000000000010100"; -- lw pego o conteudo de r0 + 5 e coloco em r2
+		--tmp(13) := b"100011_00000_00100_0000000000001100"; -- lw pego o conteudo de r0 + 6 e coloco em r4
 		--tmp(14) := b"000000_00010_00100_01000_00000_100000"; --add r2+r4 no r8
-		--tmp(15) := b"000000_00010_01000_00100_01001_100010"; --sub r2-r8 no r9
+		--tmp(15) := b"000000_00010_01000_00100_01001_100100"; --sub r2-r8 no r9
 		return tmp;
 	end init_rom;	 
 
